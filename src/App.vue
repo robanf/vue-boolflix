75c32f04c8bc63cd8d5be460752a5e1000e276d3
Roberto <robertoanfuso98@gmail.com>
2021-07-12 18:17:37 +0200

    push di rito

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Mainfilm/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Mainfilm from './components/Mainfilm.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Mainfilm
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/style/general.scss';
 </style>
---
+++ src/components/Mainfilm.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <div v-for="(film,index) in filmlist" :key="index">
+          <div class="film-box">
+          {{film.title}}
+          {{film.original_title}}
+          {{film.original_language}}
+          {{film.vote_average}}
+          </div>
+
+      </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+    data(){
+        return{
+            apiUrl: 'https://api.themoviedb.org/3/movie/popular?api_key=e074c01e562b214f49b0d0b915aa74f1',
+            filmlist:[],
+        }
+    },
+    created(){
+        this.getFilms();
+    },
+    methods:{
+        getFilms(){
+            axios
+                .get(this.apiUrl)
+                .then(response =>{
+                    this.filmlist= response.data.results;
+                    console.log(this.filmlist);
+                })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+    .film-box{
+        display: inline-flex;
+        flex-direction: initial;
+        height: 200px;
+        width: 200px;
+    }
+</style>
